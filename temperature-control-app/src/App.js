137d1487fe0db60b1181107ffc41aeff572f2fda
sumitranjan <sumitranjan.ind@gmail.com>
2022-07-15 23:52:49 +0530

    completed

---
+++ temperature-control-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Temperature from './Components/Temperature';
+
+function App() {
+  return (
+    <div className="App">
+      <Temperature/>
+    </div>
+  );
+}
+
+export default App;
